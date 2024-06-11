0d6a8428deee702f536bd80bca66e4ec9fa3767e
WooSeungwon <wsw0922>
2024-06-11 22:24:21 +0900

    프로필 수정 구현 -우승원-

--- lib/screens/my_info/info.dart
+++ lib/screens/my_info/info.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -6,6 +8,7 @@ import 'package:dongpo_test/main.dart';
 import 'package:dongpo_test/screens/login/kakao_naver_login.dart';
 import 'package:dongpo_test/screens/login/login.dart';
 import 'package:dongpo_test/screens/login/login_view_model.dart';
+import 'package:image_picker/image_picker.dart';
 
 
 class MyPage extends StatefulWidget {
@@ -19,6 +22,13 @@ class _MyPageState extends State<MyPage> {
   final loginViewModel = LoginViewModel(KakaoNaverLogin());
   static final storage = FlutterSecureStorage();
   bool isLogouted = false;
+
+  // 프로필 사진 수정 관련
+  final picker = ImagePicker();
+  XFile? image;
+
+  //버튼 활성화 비활성화를 위한 value값
+  int value = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +56,7 @@ class _MyPageState extends State<MyPage> {
                 children: [
                   // 프로필 사진
                   CircleAvatar(
-                    radius: 64,
+                    radius: 48,
                     backgroundImage: AssetImage('assets/images/profile_img1.jpg'),
                   ),
                   SizedBox(width: 16), // 간격 조정
@@ -69,7 +79,7 @@ class _MyPageState extends State<MyPage> {
                             style: TextStyle(
                               fontSize: 16,
                               fontWeight: FontWeight.w600,
-                              color: Color(0xFFF15A2B), // 텍스트 색상 유지
+                              color: Color(0xFFF15A2B),
                             ),
                           ),
                         ),
@@ -115,7 +125,7 @@ class _MyPageState extends State<MyPage> {
                 ),
               ),
             ),
-            SizedBox(height: 64), // 간격 조정
+            SizedBox(height: 24), // 간격 조정
             // 등록한 가게, 칭호, 선물함
             Container(
               padding: EdgeInsets.symmetric(horizontal: 24, vertical : 24),
@@ -123,33 +133,75 @@ class _MyPageState extends State<MyPage> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   // 등록한 가게
-                  Text(
-                    '등록한 가게',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: Color(0xFF767676),
-                    ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '0',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF767676),
+                        ),
+                      ),
+                      SizedBox(height: 8,),
+                      Text(
+                        '등록한 가게',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF767676),
+                        ),
+                      ),
+                    ],
                   ),
                   Spacer(),
                   // 칭호
-                  Text(
-                    '칭호',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: Color(0xFF767676),
-                    ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '0',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF767676),
+                        ),
+                      ),
+                      SizedBox(height: 8,),
+                      Text(
+                        '칭호',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF767676),
+                        ),
+                      ),
+                    ],
                   ),
                   Spacer(),
                   // 선물함
-                  Text(
-                    '선물함',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: Color(0xFF767676),
-                    ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '0',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF767676),
+                        ),
+                      ),
+                      SizedBox(height: 8,),
+                      Text(
+                        '선물함',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF767676),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -315,46 +367,166 @@ class _MyPageState extends State<MyPage> {
       ),
     );
   }
-  void showEditProfileBottomSheet(BuildContext context){
+  void showEditProfileBottomSheet(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height; // 현재 화면 높이
-    final bottomSheetHeight = screenHeight * 0.5; // 화면 높이의 50%
+    final bottomSheetHeight = screenHeight * 0.45; // 화면 높이의 50%
+
+    // TextEditingController를 사용하여 초기값 설정
+    final TextEditingController nicknameController = TextEditingController(text: "사용자1");
+    String nickname = nicknameController.text;
+
     showModalBottomSheet(
+      isScrollControlled: true, // 키보드가 올라올 때 바텀시트가 따라 올라감
       context: context,
-      builder: (context) {
-        return Container(
-          height: bottomSheetHeight,
-          width: double.infinity,
-          margin: EdgeInsets.all(24),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(12), topRight: Radius.circular(12)),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Text(
-                    "프로필 편집",
-                    style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.w600,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return Padding(
+              padding: EdgeInsets.only(
+                bottom : MediaQuery.of(context).viewInsets.bottom,
+              ),
+              child: GestureDetector(
+                onTap: (){
+                  FocusScope.of(context).unfocus(); //
+                },
+                child: Container(
+                  height: bottomSheetHeight,
+                  width: double.infinity,
+                  margin: EdgeInsets.all(24),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(12), topRight: Radius.circular(12)),
+                  ),
+                  child: SafeArea(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "프로필 편집",
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Spacer(),
+                              IconButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                icon: Icon(
+                                  CupertinoIcons.xmark,
+                                  size: 24,
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 40,),
+                          GestureDetector(
+                            onTap: () async {
+                              final XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
+                              if (pickedFile != null) {
+                                setState(() {
+                                  image = pickedFile;
+                                  value = 1;
+                                  logger.d("image path : ${image!.path}");
+                                  logger.d("image value : ${value}");
+                                });
+                              }
+                            },
+                            child: Stack(
+                              children: [
+                                CircleAvatar(
+                                  radius: 40, // 80 / 2
+                                  backgroundImage: image != null ? FileImage(File(image!.path)) : AssetImage('assets/images/profile_img1.jpg') as ImageProvider,
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.white,
+                                    ),
+                                    padding: EdgeInsets.all(4),
+                                    child: Icon(
+                                      Icons.camera_alt,
+                                      color: Color(0xFF767676),
+                                      size: 24,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 16,),
+                          SizedBox(
+                            height: 44,
+                            width: double.infinity,
+                            child: TextField(
+                              controller: nicknameController, // TextEditingController를 연결
+                              onChanged: (text) {
+                                setState(() {
+                                  nickname = text;
+                                  value = 1;
+                                  logger.d("image value : ${value}");
+                                });
+                              },
+                              decoration: InputDecoration(
+                                labelText: "닉네임",
+                                hintText: '7글자까지 입력 가능해요',
+                                labelStyle: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xFF767676),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                                  borderSide: BorderSide(width: 1, color: Color(0xFF767676),)
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                                  borderSide: BorderSide(width: 1, color: Color(0xFF767676),)
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 24),
+                          SizedBox(
+                            height: 44,
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  splashFactory: (value == 0)
+                                      ? NoSplash.splashFactory
+                                      : InkSplash.splashFactory,
+                                  // 수정이 있을 경우 버튼 활성화
+                                  backgroundColor:
+                                  (value == 1) ? Color(0xffF15A2B) : Color(0xFFF4F4F4),
+                                  minimumSize: Size(double.infinity, 40),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(Radius.circular(12)))),
+                              onPressed: () {
+                                // 프로필 수정 로직 구현
+                                (value == 0) ? null : logger.d("저장 완");
+                              },
+                              child: Text(
+                                '저장',
+                                style: TextStyle(
+                                    color: (value == 1) ? Colors.white : Color(0xFF767676)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  Spacer(),
-                  IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(
-                        CupertinoIcons.xmark,
-                        size: 24,
-                      )
-                  )
-                ],
+                ),
               ),
-            ],
-          ),
+            );
+          },
         );
       },
     );
